d98895fd5799859f6d43211169fbea3d88f2f86c
Karolina Okla <karolinaokla95@gmail.com>
2020-12-02 11:40:18 +0100

    Fix initAccordion

--- src/js/script.js
+++ src/js/script.js
@@ -80,7 +80,7 @@
       const thisProduct = this;
 
       /*find the clickable trigger (the element that should react to clicking) */
-      const clickableTrigger = thisProduct.querySelector(select.menuProduct.clickable);
+      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable);
 
       /* START: add event listener to clickable trigger on event click */
       clickableTrigger.addEventListener('click', function(event){
@@ -89,13 +89,16 @@
         /* find active product (product that has active class) */
         const activeProducts = document.querySelectorAll(select.all.menuProductsActive);
         /* if there is active product and it's not thisProduct.element, remove class active from it */
+        for(let activeProduct of activeProducts){
           if (activeProduct != thisProduct.element){
             activeProduct.classList.remove('active');
           }
+        }
         /* toggle active class on thisProduct.element */
         thisProduct.element.classList.toggle('active');
       });
     }
+  }
 
   const app = {
     initMenu: function(){
@@ -106,6 +109,7 @@
         new Product(productData, thisApp.data.products[productData]);
       }
     },
+
     initData: function(){
       const thisApp = this;
 
