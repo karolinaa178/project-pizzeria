de2577d9415588e9258dd2fc017af5e9d87ec29c
Karolina Okla <karolinaokla95@gmail.com>
2020-12-08 17:41:13 +0100

    Fix amountWidget

--- src/js/script.js
+++ src/js/script.js
@@ -143,6 +143,7 @@
 
       // set price to default price
       let price = thisProduct.data.price;
+      thisProduct.priceSingle = thisProduct.data.price;
 
       // for every category (param)...
       for(let paramId in thisProduct.data.params) {
@@ -150,36 +151,44 @@
         const param = thisProduct.data.params[paramId];
         console.log(paramId, param);
 
-        // for every option in this category
-        for(let optionId in param.options) {
-        // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
-          const option = param.options[optionId];
-          console.log(optionId, option);
+        for(let paramId in thisProduct.data.params) {
+          // determine param value, e.g. paramId = 'toppings', param = { label: 'Toppings', type: 'checkboxes'... }
+          const param = thisProduct.data.params[paramId];
+          console.log(paramId, param);
 
-          //check if there is param with a name of paramId in formData and if it includes optionId
-          const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
-          if(optionSelected) {
-            //check if the opriin is not default
-            if(!option.default){
-              // add option price to price variable
-              price += option.price;
+          // for every option in this category
+          for(let optionId in param.options) {
+            // determine option value, e.g. optionId = 'olives', option = { label: 'Olives', price: 2, default: true }
+            const option = param.options[optionId];
+            const optionSelected = formData[paramId] && formData[paramId].includes(optionId);
+            console.log(optionId, option);
+            if (optionSelected) {
+              if (!option.default == true) {
+                price = price + option.price;
+              }
             }
-          } else if(option.default){
-            //reduce price variable
-            price -= option.price;
-          }
-          const image = thisProduct.imageWrapper.querySelector('.' + paramId + '-' + optionId);
-          if(optionSelected && image !== null){
-            image.classList.add(classNames.menuProduct.imageVisible);
-          } else if(!optionSelected && image !== null){
-            image.classList.remove(classNames.menuProduct.imageVisible);
+            else {
+              if (option.default == true) {
+                price = price - option.price;
+              }
+            }
+            const optionImages = thisProduct.imageWrapper.querySelectorAll('.' + paramId + '-' + optionId);
+            for (let optionImage of optionImages) {
+              if (optionSelected) {
+                optionImage.classList.add(classNames.menuProduct.imageVisible);
+              }
+              else {
+                optionImage.classList.remove(classNames.menuProduct.imageVisible);
+              }
+            }
           }
         }
-        /* multiply price by amount */
-        price *= thisProduct.amountWidget.value;
-        // update calculated price in the HTML
-        thisProduct.priceElem.innerHTML = price;
       }
+      /* multiply price by amount */
+      price *= thisProduct.amountWidget.value;
+      // update calculated price in the HTML
+      thisProduct.priceElem.innerHTML = price;
+
     }
     initAmountWidget(){
       const thisProduct = this;
